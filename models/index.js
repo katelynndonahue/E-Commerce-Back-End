d89d36d39b0d0bff0c5a3c99982289b62f35508d
k.donahue45@yahoo.com <k.donahue45@yahoo.com>
2022-01-24 21:12:05 -0500

    edits to routes and server.js

--- models/index.js
+++ models/index.js
@@ -3,14 +3,48 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { Model } = require('../config/connection');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "Cascade"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.hasMany(Product, {
+  through: ProductTag,
+  as: "tags",
+  foreignKey: "category_id"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "products",
+  foreignKey: "tag_id"
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id"
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tag_id"
+});
+
+Tag.hasMany(ProductTag, {
+  foreignKey: "tag_id"
+});
+
+Product.hasMany(ProductTag, {
+  foreignKey: "product_id"
+});
 
 module.exports = {
   Product,
